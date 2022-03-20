7d033a14b5159243f3468d7e261d736b60085b7c
Lam <ngonl@garena.com>
2022-03-20 20:31:19 +0800

    UPD: store values

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
-import { FC } from "react";
-import { Route, Routes } from "react-router-dom";
-
-import Home from "./pages/Home";
+import { FC, useEffect, useState } from "react";
+import { useStore } from "@/store";
+import { wallpapers } from "@/shared/constants";
+import TopMenu from "@/components/TopMenu";
+import Dock from "@/components/Dock";
+import Title from "@/components/Title";
+import LoadingScreen from "@/components/LoadingScreen";
 
 const App: FC = () => {
+  const [loading, setLoading] = useState(false);
+  const wallpaperId = useStore((state) => state.wallpaperId);
+  const wallpaper = wallpapers[wallpaperId].url;
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
+
   return (
-    <Routes>
-      <Route index element={<Home />} />
-    </Routes>
+    <div className="app w-screen h-screen bg-black">
+      <Title value="MacOS Web" />
+      {loading ? (
+        <LoadingScreen />
+      ) : (
+        <img
+          id="wallpaper"
+          className="h-screen w-screen fixed top-0 left-0 object-cover -z-2 animate-fade-in"
+          src={wallpaper}
+        />
+      )}
+
+      <TopMenu />
+      <Dock />
+    </div>
   );
 };
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,21 +1,34 @@
 import create from "zustand";
+import { DockMenu } from "@/shared/constants";
 
 interface Store {
-  currentUser: null | undefined;
   wallpaperId: number;
   themeId: number;
+  maxWindowId: number;
+  windowList: Record<string, number>;
 
-  setCurrentUser: (user: any) => void;
-  updateWall: () => void;
+  changeWallpaper: (id: number) => void;
+  changeTheme: (id: number) => void;
 }
 
 export const useStore = create<Store>((set: any) => ({
-  currentUser: undefined,
   wallpaperId: Number(localStorage.getItem("WALLPAPER")) || 6,
   themeId: Number(localStorage.getItem("THEME")) || 0,
+  maxWindowId: 0,
+  windowList: DockMenu.filter((item) => item.type !== "link").reduce(
+    (acc, current) => {
+      acc[current.name] = 0;
+      return acc;
+    },
+    {} as any
+  ),
 
-  setCurrentUser: (user: any) => set({ currentUser: user }),
-  changeWallpaper: (id: number) => set({ wallpaperId: id }),
-  changeTheme: (id: number) => set({ themeId: id }),
-  updateWall: () => set({ wallpaperId: 0 }),
+  changeWallpaper: (id: number) => {
+    set({ wallpaperId: id });
+    localStorage.setItem("WALLPAPER", id.toString());
+  },
+  changeTheme: (id: number) => {
+    set({ themeId: id });
+    localStorage.setItem("THEME", id.toString());
+  },
 }));
